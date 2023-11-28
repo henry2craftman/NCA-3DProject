d9baf716dc0b99264438403fa178adbb2a851e0e
Henry <henry2craftman@gmail.com>
2023-11-28 17:03:53 +0900

    fix: CameraRotate.cs 수정중

---
+++ Assets/Scripts/CameraRotate.cs
@@ -0,0 +1,53 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 1. 사용자의 마우스 입력을 받아서 고개(카메라)를 돌린다.
+// 2. 키보드 3번 키를 누르면 3인칭 뷰로 전환한다.
+public class CameraRotate : MonoBehaviour
+{
+    [SerializeField] bool isThirdPersonView = false;
+    [SerializeField] GameObject thirdPersonPosition;
+    [SerializeField] float speed = 10;
+    float mouseX, mouseY;
+    Vector3 originCamPos;
+
+    private void Start()
+    {
+        originCamPos = transform.localPosition;
+    }
+
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            isThirdPersonView = !isThirdPersonView;
+
+            if (isThirdPersonView)
+            {
+                transform.SetParent(thirdPersonPosition.transform);
+                transform.localPosition = Vector3.zero;
+                transform.localRotation = Quaternion.identity;
+            }
+            else
+            {
+                transform.SetParent(thirdPersonPosition.transform.parent);
+                transform.localPosition = originCamPos;
+                transform.localRotation = Quaternion.identity;
+
+            }
+        }
+
+        float horizontalInput = Input.GetAxis("Mouse X");
+        float verticalInput = Input.GetAxis("Mouse Y");
+
+        mouseX += horizontalInput * speed * Time.deltaTime;
+        mouseY += verticalInput * speed * Time.deltaTime;
+
+        mouseY = Mathf.Clamp(mouseY, -90, 90);
+
+        Vector3 rotationValue = new Vector3(-mouseY, mouseX, 0);
+
+        transform.eulerAngles = rotationValue;
+    }
+}
---
+++ Assets/Scripts/PlayerMove.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 플레이어가 사용자의 키보드 입력을 받아 특정 속도로 이동한다.
+public class PlayerMove : MonoBehaviour
+{
+    [SerializeField] float speed = 10;
+    [SerializeField] float gravity = -10;
+    [SerializeField] float jumpPower = 10;
+    float yVelocity;
+    bool isJumping = true;
+    CharacterController controller;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+
+        Vector3 direction = new Vector3(horizontalInput, 0, verticalInput);
+
+        direction = Camera.main.transform.TransformDirection(direction);
+
+        if(isJumping && controller.collisionFlags == CollisionFlags.Below)
+        {
+            isJumping = false;
+
+            yVelocity = 0;
+        }
+
+        if(Input.GetButtonDown("Jump") && !isJumping) // Space bar를 누르면
+        {
+            yVelocity = jumpPower;
+            isJumping = true;
+        }
+
+        yVelocity += gravity * Time.deltaTime;
+        direction.y = yVelocity;
+
+        controller.Move(direction * speed * Time.deltaTime);
+    }
+}
