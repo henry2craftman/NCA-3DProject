94cdd9be73516576913d9357bd3651b14bb74391
Henry <henry2craftman@gmail.com>
2023-12-05 14:28:11 +0900

    feat: 캐릭터 에셋 추가, FSM-Animation 연결, OverSphere 기능 추가
    - 캐릭터 에셋 추가
    - FSM-Animation 연결
    - OverSphere 기능 추가

--- Assets/CharacterFSM.cs
+++ Assets/CharacterFSM.cs
@@ -12,15 +12,17 @@ using UnityEngine;
 // 3.Behave: 반응상태(사용자가 특정 행동을 했을 때)
 public class CharacterFSM : MonoBehaviour
 {
-    enum NPCStage
+    public enum NPCState
     {
         Idle,
         Talk,
         Return,
-        follow
+        Follow,
+        Turn,
+        Dance
     }
 
-    [SerializeField] NPCStage state = NPCStage.Idle;
+    [SerializeField] NPCState state = NPCState.Idle;
     [SerializeField] float speed = 3;
     [SerializeField] float recognizeDistance = 5;
     [SerializeField] Color[] recognizeColors = new Color[4] { new Color(0, 0.45f, 1f, 1f), Color.red, Color.green, Color.blue };
@@ -35,6 +37,7 @@ public class CharacterFSM : MonoBehaviour
     private float yVelocity;
     private float gravity = -10;
     Vector3 originPos;
+    Animator animator;
 
     void Start()
     {
@@ -42,6 +45,7 @@ public class CharacterFSM : MonoBehaviour
         mat = GetComponent<MeshRenderer>().material;
         characterController = GetComponent<CharacterController>();
         originPos = transform.position;
+        animator = GetComponentInChildren<Animator>();
 
         UpdateNPCDialogue();
     }
@@ -64,26 +68,53 @@ public class CharacterFSM : MonoBehaviour
     {
         switch(state)
         {
-            case NPCStage.Idle:
+            case NPCState.Idle:
                 Idle();
                 break;
-            case NPCStage.Talk:
+            case NPCState.Talk:
                 Talk();
                 break;
-            case NPCStage.Return:
+            case NPCState.Return:
                 Return();
                 break;
-            case NPCStage.follow:
+            case NPCState.Follow:
                 Follow();
                 break;
+            case NPCState.Dance:
+                Dance();
+                break;
+        }
+    }
+
+    public void OnStateChangeEvent(NPCState _state)
+    {
+        state = _state;
+        animator.SetTrigger("Dance");
+    }
+
+    // 3초 동안 춤을 춘다.
+    private void Dance()
+    {
+        currentTime += Time.deltaTime;
+        if(currentTime > 3)
+        {
+            currentTime = 0;
+
+            state = NPCState.Return;
+            animator.SetTrigger("DanceToReturn");
         }
     }
 
     // 플레이어를 따라간다.
     private void Follow()
     {
+        // 캐릭터의 Forward가 플레이어를 향하고 있지 않다면, Turn 애니메이션 시작
+        // 캐릭터의 Forward 방향을 플레이어의 방향으로.
+
         float distance = (player.transform.position - transform.position).magnitude;
         Vector3 direction = (player.transform.position - transform.position).normalized;
+
+        transform.forward = direction;
 
         yVelocity += gravity * Time.deltaTime;
         direction.y = yVelocity;
@@ -92,9 +123,10 @@ public class CharacterFSM : MonoBehaviour
 
         if (distance > recognizeDistance)
         {
-            mat.color = recognizeColors[(int)NPCStage.Return];
+            mat.color = recognizeColors[(int)NPCState.Return];
 
-            state = NPCStage.Return;
+            state = NPCState.Return; // Trigger(Follow -> Return)
+            animator.SetTrigger("FollowToReturn");
 
             UpdateNPCDialogue("Oh No~~ Too fast!");
         }
@@ -102,31 +134,40 @@ public class CharacterFSM : MonoBehaviour
 
     private void Return()
     {
+        // 회전 천천히 할 수 있도록 변경
         float distanceToPlayer = (player.transform.position - transform.position).magnitude;
         float distanceToOriginPos = (originPos - transform.position).magnitude;
         Vector3 direction = (originPos - transform.position).normalized;
+
+        if (distanceToPlayer < recognizeDistance)
+        {
+            mat.color = recognizeColors[(int)NPCState.Follow];
+
+            state = NPCState.Follow; // Trigger(Return -> Follow)
+            animator.SetTrigger("ReturnToFollow");
+
+            UpdateNPCDialogue("Stop! I'm here!");
+        }
+        else if (distanceToOriginPos <= 0.3f)
+        {
+            state = NPCState.Idle; // Trigger(Return -> Idle)
+            animator.SetTrigger("ReturnToIdle");
+
+            mat.color = recognizeColors[(int)NPCState.Idle];
+
+            UpdateNPCDialogue();
+
+            return;
+        }
+
+
+        transform.forward = direction;
 
         yVelocity += gravity * Time.deltaTime;
         direction.y = yVelocity;
 
         characterController.Move(direction * speed * Time.deltaTime);
-
-
-        if (distanceToPlayer < recognizeDistance)
-        {
-            mat.color = recognizeColors[(int)NPCStage.follow];
-
-            state = NPCStage.follow;
-
-            UpdateNPCDialogue("Stop! I'm here!");
-        }
-        else if(distanceToOriginPos <= 0.3f)
-        {
-            state = NPCStage.Idle;
-            UpdateNPCDialogue();
-        }
     }
-
 
     private void Talk()
     {
@@ -144,16 +185,19 @@ public class CharacterFSM : MonoBehaviour
             // 인식 범위 내로 밖으로 나가면, 원상태(Idle)로 복귀
             if (distance > recognizeDistance) 
             {
-                mat.color = recognizeColors[(int)NPCStage.Idle];
+                mat.color = recognizeColors[(int)NPCState.Idle];
 
-                state = NPCStage.Idle;
+                state = NPCState.Idle; // Trigger(Talk -> Idle)
+                animator.SetTrigger("TalkToIdle");
             }
             else
             {
                 // 플레이어를 따라간다.
-                mat.color = recognizeColors[(int)NPCStage.follow];
+                mat.color = recognizeColors[(int)NPCState.Follow];
 
-                state = NPCStage.follow;
+                state = NPCState.Follow; // Trigger(Talk -> Follow)
+                animator.SetTrigger("TalkToFollow");
+
                 UpdateNPCDialogue("Following player");
             }
         }
@@ -161,23 +205,29 @@ public class CharacterFSM : MonoBehaviour
 
     private void Idle()
     {
-        // 사용자의 입력이 들어온다면, 아래로 진행
-
-
         // 만약에 플레이어가 가까이 오면,
         float distance = Vector3.Distance(transform.position, player.position);
-        //distance = (transform.position - player.position).magnitude;
 
         if(distance < recognizeDistance) // 인식 범위 내로 들어오면
         {
             // Hello 말하고 상태를 변경
-            mat.color = recognizeColors[(int)NPCStage.Talk];
+            mat.color = recognizeColors[(int)NPCState.Talk];
 
-            state = NPCStage.Talk;
+            state = NPCState.Talk; // Trigger(Idle -> Talk)
+            animator.SetTrigger("IdleToTalk");
 
             transform.LookAt(player);
 
             UpdateNPCDialogue("Hello, Welcome!");
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.gameObject.layer == 6) // Bomb Layer
+        {
+            state = NPCState.Dance;
+            animator.SetTrigger("Dance");
         }
     }
 }
--- Assets/Scripts/BombAction.cs
+++ Assets/Scripts/BombAction.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,6 +7,8 @@ using UnityEngine;
 public class BombAction : MonoBehaviour
 {
     [SerializeField] GameObject bombEffect;
+    [SerializeField] float radius = 5;
+    [SerializeField] LayerMask layerMask;
 
     private void OnCollisionEnter(Collision collision)
     {
@@ -13,6 +16,25 @@ public class BombAction : MonoBehaviour
 
         effect.transform.position = this.transform.position;
 
+        // 어떤 물체들이 있는지 확인하고 싶다.
+        CheckColliders();
+
         Destroy(this.gameObject);
+    }
+
+    private void CheckColliders()
+    {
+        Collider[] colliders = Physics.OverlapSphere(this.transform.position, radius, layerMask);
+
+        foreach (Collider collider in colliders)
+        {
+            collider.GetComponent<CharacterFSM>().OnStateChangeEvent(CharacterFSM.NPCState.Dance);
+        }
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = new Color(0, 0.45f, 1, 0.5f);
+        Gizmos.DrawSphere(transform.position, radius);
     }
 }
